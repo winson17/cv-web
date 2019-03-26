5d119133fd80da73033514f31128f873dcab6be4
Winson <winson.au@ynov.com>
2019-03-26 15:20:27 +0100

    new website

--- index.php
+++ index.php
@@ -37,10 +37,10 @@
 			<h1 class="logo"><a href="index.html">Menu</a></h1>
 			<i class="fa fa-times menu-close"></i>
 			<a href="#home" class="js-scrollTo">Home</a>
-			<a href="#about" class="js-scrollTo">About me</a>
+			<a href="#about" class="js-scrollTo">A propos de moi</a>
 			<a href="#portfolio" class="js-scrollTo">Portfolio</a>
-      <a href="#hobbies" class="js-scrollTo">Hobbies</a>
-			<a href="#services" class="js-scrollTo">Habilities</a>
+      <a href="#hobbies" class="js-scrollTo">Centres d'interêts</a>
+			<a href="#services" class="js-scrollTo">Compétences</a>
 			<a href="#contact" class="js-scrollTo">Contact</a>
 			<a href="https://www.facebook.com/winson.au.71?ref=bookmarks"><i class="fa fa-facebook"></i></a>
 			<a href="https://twitter.com/Winsonau77"><i class="fa fa-twitter"></i></a>
@@ -73,16 +73,16 @@
 					<div class="name-label"></div>
 				</div><! --/col-lg-4-->
 				<div class="col-lg-8 name-desc">
-					<h2>About me</h2>
+					<h2>A Propos de moi</h2>
 					<div class="name-zig"></div>
 
 					<div class="col-md-6">
 						<p>Bonjour à tous,
-						je m'appelle AU Winson ,j'ai 19ans, actuellement étudiant à Ynov Ingésup. j'ai eu mon baccalauréat Scientifique en Juillet 2017,mention Anglais Européen, specialitée informatique et science du numérique, j'ai aussi le permis B. J'ai été President de la maison des lycéens,une asscociation au sein de mon ancien Lycée.je suis serieux, sportif et investi dans tout ce que je fais. </p>
+						je m'appelle AU Winson ,j'ai 19ans, actuellement étudiant à Ynov Ingésup. j'ai eu mon baccalauréat Scientifique en Juillet 2017,mention Anglais Européen, specialitée informatique et science du numérique, j'ai aussi le permis B. Je suis serieux, sportif et investi dans tout ce que je fais. </p>
 					</div>
 					<div class="col-md-6">
 						<p>Hello,
-						My name is AU Winson,I'm 19 years old, I'm student at Ynov Ingésup. i've got my Scientific Baccalaureat on July 2017 with mention : European Section, Speciality data processing and digital science.I was Presedient of "maison des lycéens", an association of my highschool. I'm serious, athletic and invested on evrything what I'm doing. </p>
+						My name is AU Winson,I'm 19 years old, I'm student at Ynov Ingésup. i've got my Scientific Baccalaureat on July 2017 with mention : European Section, Speciality data processing and digital science. I'm serious, athletic and invested on evrything what I'm doing. </p>
 					</div>
 
 				</div><! --/col-lg-8-->
@@ -105,9 +105,10 @@
 	                    <div class="project">
 	                        <div class="photo-wrapper">
 	                            <div class="photo">
-	                            	<a class="fancybox" href="assets/img/portfolio/Workshop2019.jpeg"><img class="img-responsive" src="assets/img/portfolio/Workshop2019.jpeg" alt=""></a>
+	                            	<a class="fancybox" href="assets/img/portfolio/thumbnail.png"><img class="img-responsive" src="assets/img/portfolio/thumbnail.png" alt=""></a>
                                 <div class="service">
                                   <p class="text"><b> Vainqueur des Workshop de Paris 2019 </b> </br>
+                                    UXES <br>
                                       - Projet 1 (BlastPro Series) :
                                       - Briser la barrière virtuelle entre spectateurs et joueurs </br>
                                       - Projet 2 (Nice):
@@ -132,6 +133,7 @@
                                 <a class="fancybox" href="assets/img/portfolio/logo.png"><img class="img-responsive" src="assets/img/portfolio/logo.png" alt=""></a>
                                 <div class="service">
                                   <p class="text"> <b> Ydays2019 </b> </br>
+                                    Safe Ride <br>
                                       - Projet : Conception d'une application permettant aux utilisateurs de rentrer chez eux via un itinéraire dit "safe" </br>
                                       Technologie : Git, c#(framework: xamarion), SQL,
                                    </p>
@@ -150,6 +152,7 @@
 	                            	<a class="fancybox" href="assets/img/portfolio/Yb.png"><img class="img-responsive" src="assets/img/portfolio/Yb.png" alt=""></a>
                                 <div class="service">
                                   <p class="text"> <b> Ydays2018 </b> </br>
+                                    Yboard <br>
                                       - Projet : Conception d'un dashboard pour les étudiants </br>
                                       Technologie : PHP, Html, css
                                    </p>
@@ -173,7 +176,7 @@
   <div id="portfoliowrap">
     <div class="container">
       <div class="row">
-        <h1>Hobbies</h1>
+        <h1>Centres d'interêts</h1>
         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
           <div class="project-wrapper">
                       <div class="project">
@@ -270,26 +273,26 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-8-offset-2 centered">
-					<h1>Habilities</h1>
+					<h1>Compétences</h1>
 				</div><!-- /col-lg-8 -->
 			</div><! --/row -->
 
 			<div class="row mt">
 				<div class="col-lg-3 service">
-					<i class="fa fa-windows"></i><p>pack office 365<br/></p>
-					<p class="text">Word2016,Excel2016,Powerpoint2016 etc....</p>
+					<i class="fa fa-windows"></i><p>Software<br/></p>
+					<p class="text">Pack Office 360, <br> Photoshop</p>
 				</div>
 				<div class="col-lg-3 service">
-					<i class="fa fa-laptop"></i><p>Programation<br/></p>
-					<p class="text">Python, Html, CSS, PHP, C#,  Javascript, Node.js, Vue.js, SQL, PostrgeSql, SQLServer.</p>
+					<i class="fa fa-laptop"></i><p>Hardware<br/></p>
+					<p class="text">Python, C#, <br/> Html, CSS, PHP, <br/> Javascript, Node.js, Vue.js,<br/> SQL, PostrgeSql, SQLServer, MongoDB.</p>
 				</div>
 				<div class="col-lg-3 service">
-					<i class="fa fa-globe"></i><p>Language<br/></p>
-					<p class="text">Français: langue maternelle(French: principal language), Anglais courant( English regular pratice), Chinois: langue maternelle(Chinese: principal language), Espagnol: bon niveau(Spanish: good level).</p>
+					<i class="fa fa-globe"></i><p>Langues<br/></p>
+					<p class="text">Français: langue maternelle(French: principal language),<br> Anglais courant( English regular pratice), <br> Chinois Cantonais: langue maternelle(Chinese: principal language),<br> Espagnol: bon niveau(Spanish: good level).</p>
 				</div>
 				<div class="col-lg-3 service">
-					<i class="fa fa-user"></i><p>Socials<br/></p>
-					<p class="text">Facebook, Twitter, Linkedln, Instagram.</p>
+					<i class="fa fa-user"></i><p>Sociales<br/></p>
+					<p class="text">Facebook,<br> Twitter,<br> Linkedln,<br> Instagram.</p>
 				</div>
 			</div><! --/row -->
 
@@ -310,9 +313,9 @@
 						FRANCE.</small>
 					</p>
 					<p>
-						<small>Tel.06.79.88.86.00<br/>
-						Mail:winsonau772@gmail.com ou <br/>
-						winson.au@ynov.com
+						<small>Tel : +33(0)6.79.88.86.00<br/>
+						Mail: winson.au@ynov.com ou <br/>
+						winsonau772@gmail.com
 					</p>
 
 				</div>
